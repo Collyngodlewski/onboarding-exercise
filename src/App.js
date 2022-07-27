642d36da1540b1da6c1f6b8d2bad5a363a6d8422
Collyngodlewski <collyngodlewski@gmail.com>
2022-07-27 17:16:27 -0400

    Did basic reqs, added a filter for title.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import React from "react";
+
 import logo from "./queeery-logo.png";
 import "./App.css";
+import { movies }  from "./movies";
+import { useState } from "react";
+
+
+
 
 function App() {
+  const [liked, setLiked] = useState(0);
+  const [disliked, setDisliked] = useState(0);
+  const [searchMovie, setSearchMovie] = useState('');
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> <br />
-          have fun and be creative!
-        </p>
+          Welcome to Queeery's Movies
+       </p>
+       
+       <div className="Search-bar">
+          <input type="text" placeholder="Search movies..." onChange={event => {setSearchMovie(event.target.value)}}/>
+     
+
+            <div className="Movies">
+              { movies.filter((val)=>{
+                if (searchMovie == ""){
+                  return val
+                 } else if (val.title.toLowerCase().includes(searchMovie.toLowerCase())){
+                  return val
+                 }
+              }).map(movie => {
+                return(
+                  <div key={movie.id} className="Movie-box">
+                  <h2>{movie.title}</h2>
+                  <p>{movie.category}</p>
+                  <img src = {movie.url} alt="Movie poster art"/>
+                  <p> Likes: {movie.likes} Dislikes: {movie.dislikes} </p>
+                  <button className="Buttons" key={movie.id} onClick={() => setLiked(movie.likes++)}>Like</button>
+                  <button className="Buttons" key={movie.id} onClick={() => setDisliked(movie.dislikes++)}>Dislike</button>
+                  </div>
+                )
+              })}
+              
+            </div>
+      </div>
+       
       </header>
     </div>
   );
